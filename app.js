9f8507ad10e749c570f38547a9e8be5604202b97
Randy's Mac <randydavidl78@gmail.com>
2018-05-08 23:23:09 -0400

    installed dependencies, added code to start server in app.js

---
+++ app.js
@@ -0,0 +1,28 @@
+/* jshint esversion: 6 */
+/*
+ * Main app for Rest API
+ * @author Randy Layne
+ * @version 1.0.0
+ */
+
+ // Import express, app start point
+ const express = require('express');
+ const app = express();
+
+ /* Project dependencies middleware */
+ // used to create json returns and recieves
+ const jsonParser = require("body-parser").json;
+ // used for console logs (mostly for color to help debugging)
+ const logger = require("morgan");
+
+ // Custom middleware imports
+
+
+ /* app starting point */
+ 
+
+ /* create a server to run app */
+ const port = process.env.port || 3030;
+ app.listen(port, () => {
+   console.log(`Server started on port ${port}`);
+ });
