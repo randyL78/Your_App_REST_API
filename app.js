057b3a64ce25fa1b77cef81a61b8b941e7a31aa3
Randy's Mac <randydavidl78@gmail.com>
2018-05-10 23:09:03 -0400

    added CORS to header of responses in app.js

--- app.js
+++ app.js
@@ -25,6 +25,13 @@ const errors = require("./errors");
 app.use(logger("dev"));
 app.use(jsonParser());
 
+/* add CORS access */
+app.all('/', function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  next();
+});
+
 // handle routes
 app.use("/rest", routes);
 
